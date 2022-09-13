f4b339940971d08810e3f0d1166c530038ffdcb1
fso <fso@netic.dk>
2022-09-13 14:37:56 +0200

    oprettet kommentarer og beskrivelse

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+#mod spiller = opret kode der skal gemmes med op til 2 kopier
+
+#input af spillerens gæt
+
+#afklar hvordan det matcher op mod koden
+
+#Check om der er 2 af samme farve
+
+#giv svar tilbage og vis tavlen med alle svar
+
+import random
+random.seed(2)
+
+
+for _ in range(4):
+    value = (random.randint(0,10))
+    print(value)
+
