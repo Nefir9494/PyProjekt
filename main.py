0298fa8b01e40aaa26f077b27f86a57cc6bb2e10
fso <fso@netic.dk>
2022-09-17 21:52:00 +0200

    oprettet funktion til random + at printe de rigtige format ud

--- main.py
+++ main.py
@@ -8,12 +8,38 @@
 #Check om der er 2 af samme farve
 
 #giv svar tilbage og vis tavlen med alle svar
-
+import string
 import random
-random.seed(2)
+number_of_available_colors = 8 - 1
+board_size = 4
+alphabet = string.ascii_uppercase
+colors = []
+guess_list = []
 
 
-for _ in range(4):
-    value = (random.randint(0,10))
-    print(value)
+#shortcut to make a random nr
+def nr():
+    return random.randint(0, number_of_available_colors)
+
+
+def print_result(list_to_print):
+    print("||{}||".format(".".join(list_to_print)))
+
+
+for i in range(number_of_available_colors + 1):
+    colors.append(alphabet[i])
+
+
+print(colors)
+
+
+for i in range(board_size):
+    value = (colors[nr()])
+    guess_list.append(value)
+
+
+
+
+
+print_result(guess_list)
 
